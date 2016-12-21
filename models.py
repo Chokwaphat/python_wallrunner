df425e619b12313f0651a0a3222f461be68da6ab
Chokwaphat <chokwaphat@Chokwaphats-MacBook-Pro.local>
2016-12-21 21:10:28 +0700

    Can go left and right

--- models.py
+++ models.py
@@ -1,9 +1,11 @@
 import arcade.key
 from random import randint
+import random
 
 MAX_VY = 10
 ACCY = 1
-
+LEFT_VX = 10
+RIGHT_VX = -10
 class Model:
     def __init__(self, world, x, y, angle):
         self.world = world
@@ -14,33 +16,41 @@ class Model:
 class Man(Model):
     def __init__(self, world, x, y):
         super().__init__(world, x, y, 0)
-        print("=================")
+        # print("=================")
         self.vx = 0
         self.vy = 0
-        print("=================")
+        self.is_left = False
+        # print("=================")
+
+    def left(self):
+        self.is_left = True
+        self.vx = LEFT_VX
+
+    def right(self):
+        self.is_right = True
+        self.vx = RIGHT_VX
+
     def animate(self, delta):
         if self.vy < MAX_VY:
             self.vy += ACCY
         self.y += self.vy
-
+        if self.is_left:
+            self.x -= self.vx
 class World:
     def __init__(self, width, height):
         self.width = width
         self.height = height
-        print("sddssddsdsd11111")
         self.man = Man(self, 200, 50)
-        # self.init_platforms()
-        print("sddssddsdsd")
-        self.rock = Rock(150, 550, 0, 0)
-    # def init_platforms(self):
-    #     self.rocks = []
-    #     for p in self.platforms:
-    #         self.rocks += p.spawn_rocks()
+        self.rock = Rock(random.randrange(400), 550, 0, 0)
 
     def animate(self, delta):
         self.man.animate(delta)
 
-
+    def on_key_press(self, key, key_modifiers):
+        if key == arcade.key.LEFT:
+            self.man.left()
+        if key == arcade.key.RIGHT:
+            self.man.right()
 class Rock:
     def __init__(self, x, y, width, height):
         self.x = x
--- wallrunner.py
+++ wallrunner.py
@@ -2,7 +2,7 @@ import arcade
 from models import World
 import pyglet.gl as gl
 
-SCREEN_WIDTH = 400
+SCREEN_WIDTH = 500
 SCREEN_HEIGHT = 600
 
 class ModelSprite(arcade.Sprite):
@@ -23,7 +23,7 @@ class WallRunnerGameWindow(arcade.Window):
     def __init__(self, width, height):
         super().__init__(width, height)
 
-        arcade.set_background_color(arcade.color.GRAY)
+        arcade.set_background_color(arcade.color.GREEN)
         self.world = World(SCREEN_WIDTH, SCREEN_HEIGHT)
 
         self.man_sprite = ModelSprite('images/man.png',
@@ -43,9 +43,12 @@ class WallRunnerGameWindow(arcade.Window):
     def on_draw(self):
         arcade.set_viewport(self.world.man.x - SCREEN_WIDTH // 2,
                             self.world.man.x + SCREEN_WIDTH // 2,
-                            0, SCREEN_HEIGHT)
+                            self.world.man.y - 100,self.world.man.y + SCREEN_HEIGHT // 2,
+                            )
         arcade.start_render()
+
         self.man_sprite.draw()
+
         gl.glDisable(gl.GL_TEXTURE_2D)
         self.rock_sprite.draw()
 
